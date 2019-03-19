81dfdabcc995112bdc269e4414820bbedc7ec7fe
liuhao <965029702@qq.com>
2019-03-19 22:46:13 +0800

    ===day02基础环境搭建

--- gmall-ums/src/main/java/com/atguigu/gmall/ums/GmallUmsApplication.java
+++ gmall-ums/src/main/java/com/atguigu/gmall/ums/GmallUmsApplication.java
@@ -1,9 +1,11 @@
 package com.atguigu.gmall.ums;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.atguigu.gmall.ums.mapper")
 public class GmallUmsApplication {
 
 	public static void main(String[] args) {
